82882c3f7d2087edfabf0e9dbaf2b7c272d923fa
NEe0i <vadimnikolaev2003145@gmail.com>
2024-10-29 10:13:01 +0300

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,5 @@
+number1 = int(input("введите число"))
+number2 = int(input("введите число"))
+number3 = int(input("введите число"))
+number4 = int(input("введите число"))
+print(number1,"",number2,"",number3,"",number4)
