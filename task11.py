85dcb65b060a6abcbdd9ba6ad8efd6198214f463
NEe0i <vadimnikolaev2003145@gmail.com>
2024-10-29 10:19:34 +0300

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,8 @@
+number1 = int(input("введите число"))
+number2 = int(input("введите число"))
+number3 = int(input("введите число"))
+number4 = int(input("введите число"))
+print(number1)
+print(number2)
+print(number3)
+print(number4)
