5c01ccc445f1840afaeb441b1d37be60d74d0f15
NEe0i <vadimnikolaev2003145@gmail.com>
2024-12-10 08:39:51 +0000

    Вадим

--- task31.py
+++ task31.py
@@ -1,2 +1,9 @@
-num = int(input("Введите число: "))
-num
+num1 = int(input("Введите число: "))
+num2 = int(input("Введите число: "))
+
+
+Arithmetic_mean = (num1 + num2) / 2
+print("a)", Arithmetic_mean)
+Geometric_mean = round((num1 * num2) ** 0.5, 2)
+print("б)", Geometric_mean)
+
